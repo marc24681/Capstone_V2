ea3ec35d6277e344736f7f7a57262005dcee9b3b
Marc Gonzalez <marca_gonzalez@hotmail.com>
2023-10-27 08:20:14 -0400

    Update algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -7,16 +7,16 @@ function Submit() {
     const tablessafe = document.getElementById('container-safe');
     const tablesdanger = document.getElementById('container-danger');
 
-	const q1 = parseInt(form.elements['q1'].value);
-	const q2 = parseInt(form.elements['q2'].value);
-    const q3 = parseInt(form.elements['q3'].value);
-	const q4 = parseInt(form.elements['q4'].value);
-    const q5 = parseInt(form.elements['q5'].value);
-    const q6 = parseInt(form.elements['q6'].value);
-    const q7 = parseInt(form.elements['q7'].value);
-    const q8 = parseInt(form.elements['q8'].value); 
-    const q9 = parseInt(form.elements['q9'].value);
-    const q10 = parseInt(form.elements['q10'].value);
+		const q1 = isValid(parseInt(form.elements['q1'].value));
+	const q2 = isValid(parseInt(form.elements['q2'].value));
+    const q3 = isValid(parseInt(form.elements['q3'].value));
+	const q4 = isValid(parseInt(form.elements['q4'].value));
+    const q5 = isValid(parseInt(form.elements['q5'].value));
+    const q6 = isValid(parseInt(form.elements['q6'].value));
+    const q7 = isValid(parseInt(form.elements['q7'].value));
+    const q8 = isValid(parseInt(form.elements['q8'].value)); 
+    const q9 = isValid(parseInt(form.elements['q9'].value));
+    const q10 = isValid(parseInt(form.elements['q10'].value));
 
 	const total = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 +q10;
 console.log(total);
@@ -55,7 +55,11 @@ console.log(total);
     }
 
 }
-
+function isValid(value){
+    if(value>=1 && value <=6){
+        return value;
+    }else{return 0;}
+}
 /* Make questions appear and disapear*/
 var currentQuestion = 1;
 var totalQuestions = 10;
